224522eec7246e861f81dd7cbf946c8750ce6095
Owen Kieffer-Jones <okj579@gmail.com>
2012-03-09 11:39:43 -0500

    first commit

---
+++ settings.js
@@ -0,0 +1,33 @@
+function Settings() {
+	this.keys = {
+		next: 39, //Right arrow
+		back: 37 //Left arrow
+	}
+	this.tts = {
+		enabled: true,
+		title: true,
+		caption: true,
+		action: false,
+		options: {
+			rate: 1,
+			enqueue: true
+		}
+	}
+}
+
+function saveSettings(settings) {
+	localStorage['settings'] = JSON.stringify(settings);
+	chrome.extension.sendRequest({action:"updateSettings",settings:settings});
+}
+
+function getSettings() {
+	var settings = new Settings();
+	$.extend(true, settings, JSON.parse(localStorage['settings'] || false));
+	return settings;
+}
+
+settings = getSettings();
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+	if(request.action=='updateSettings') settings = request.settings;
+});
